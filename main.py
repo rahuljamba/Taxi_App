67e6e2e7110b5df12bccb63f3e942e1262514b4d
Rahul Jamba <rahul.jamba@routeware.com>
2026-06-11 20:11:54 +0530

    create user register post api

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Taxi App Dummy API")
+
+class User(BaseModel):
+    id: str
+    name: str | None = None
+    role: str
 
 # Dummy JSON Data
 DUMMY_USERS = [
@@ -16,3 +22,8 @@ def home():
 @app.get("/users")
 def get_users():
     return {"status": "success", "data": DUMMY_USERS}
+
+@app.post("/register_user")
+def add_users(user: User):
+    DUMMY_USERS.append({"id": user.id, "name": user.name, "role": user.role})
+    return {"status": "success", "message": f" {user.name} added sucesfully our database !!"}  
